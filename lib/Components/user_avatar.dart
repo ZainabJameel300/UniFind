0a02f54fa4fdb2c468a944a706c40daa46b8a8fd
Maryam Alshaikh <alshaiikh113@gmail.com>
2025-11-15 21:55:55 +0300

    Added User Avatar

--- lib/Components/user_avatar.dart
+++ lib/Components/user_avatar.dart
@@ -4,27 +4,21 @@ class UserAvatar extends StatelessWidget {
   final String avatarUrl;
   final double radius;
 
-  const UserAvatar({super.key, required this.avatarUrl, this.radius = 22});
+  const UserAvatar({
+    super.key, 
+    required this.avatarUrl, 
+    required this.radius
+  });
 
   @override
   Widget build(BuildContext context) {
-    if (avatarUrl.isNotEmpty) {
-      return CircleAvatar(
-        radius: radius,
-        backgroundColor: Colors.transparent,
-        backgroundImage: NetworkImage(avatarUrl),
-      );
-    } else {
-      return CircleAvatar(
-        radius: radius,
-        backgroundColor: Colors.transparent,
-        child: Icon(
-          Icons.account_circle,
-          size: radius * 2,
-          color: Colors.grey,
-        ),
-      );
-    }
-
+    return CircleAvatar(
+      radius: radius,
+      backgroundColor: Colors.grey[350],
+      backgroundImage: avatarUrl.isNotEmpty
+          ? NetworkImage(avatarUrl)
+          : const AssetImage('assets/default_avatar.png'),
+    );
   }
 }
+
